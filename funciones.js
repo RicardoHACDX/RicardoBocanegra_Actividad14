c09b8b1a9c68e32c1b0bf23ebf287cf1217bc9ec
RicardoBocanegra <ricardomendez145@gmail.com>
2020-11-21 20:52:21 -0600

    correccion

---
+++ funciones.js
@@ -0,0 +1,8 @@
+function comparar () {
+const valor1 = Number(prompt("550"));
+const valor2 = Number(prompt("673"));}
+if (valor1>valor2){
+    alert(valor1 + "Es mayor" + valor2);}
+        else{
+    alert(valor2 + "Es mayor" + valor1);
+}
